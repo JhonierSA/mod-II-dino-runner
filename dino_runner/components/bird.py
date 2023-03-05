32bf6d9a03706415000266b1286a0657e26ea4de
JhonierSA <raidjhontc@gmail.com>
2023-03-04 21:08:17 -0500

    Game 3.0

---
+++ dino_runner/components/bird.py
@@ -0,0 +1,35 @@
+from dino_runner.utils.constants import BIRD
+import random
+from pygame.sprite import Sprite
+
+class Birds(Sprite):
+    def __init__(self):
+        self.image = BIRD[1]
+        self.step = 0       
+        self.y_velocity = 10     
+        self.bird_rect = self.image.get_rect()
+        self.bird_rect_x = 1100
+        self.bird_rect_y = 100
+        self.random_choise = True
+        
+    def update(self):
+        self.fly()
+        self.step += 1
+        if self.step == 12:
+            self.step = 0
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.bird_rect_x, self.bird_rect_y))
+
+    def fly(self):
+        if self.random_choise:
+            self.bird_rect_y = random.choice((170, 180, 190, 200, 240, 260)) 
+            self.random_choise = False
+            if self.bird_rect_x <= -100:
+                self.bird_rect_x = 1100
+                self.random_choise = True
+
+        self.image = BIRD[0] if self.step < 6 else BIRD[1]
+        self.bird_rect = self.image.get_rect()
+        self.bird_rect_x -= self.y_velocity
+        
